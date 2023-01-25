96ddb852897b23e38b8324af7bae40dc6a7d28c3
Lahuraje <lahurajek@gmail.com>
2023-01-25 17:51:37 +0530

    error page added

--- taskmgmtsys/src/app/app-routing.module.ts
+++ taskmgmtsys/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PagenotfoundComponent } from './shared/error/pagenotfound/pagenotfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'dashboard',component:DashboardComponent},
+  {path:'**',component:PagenotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- taskmgmtsys/src/app/app.module.ts
+++ taskmgmtsys/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './nav/header/header.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { PagenotfoundComponent } from './shared/error/pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DashboardComponent
+    DashboardComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
